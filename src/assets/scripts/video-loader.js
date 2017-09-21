3b397d85ee748608a2f597874b841c6b0ab9ccce
Mitchell Warren <hello@liquidvisual.com>
2017-09-21 23:11:20 +1000

    Before removal of replays template + video code
    Video integrated with JS now

--- src/assets/scripts/video-loader.js
+++ src/assets/scripts/video-loader.js
@@ -5,47 +5,91 @@
 // VARIABLES
 //-----------------------------------------------------------------
 
-var $dateSelector = $('[date-selector]');
+//var $dateSelector = $('[date-selector]');
+
+// PLAYER DEFAULTS
+var player_conf = {
+	autostart: true,
+	primary: 'html5',
+	fallback: true,
+	androidhls: true,
+	width: '100%',
+    height: 421,
+    logo: {
+        file: 'profiles/swellnet/modules/features/swellnet_surfcam/assets/logo_transparent_overlay.png'
+    },
+    stagevideo: false,
+    events: {
+        onReady: function(e) {
+            window.swellnetElapsedTime = 0;
+        },
+        onTime: function(e) {
+            if ((window.swellnetElapsedTime + e.position) >= '300') {
+                this.stop();
+            }
+        },
+        onPause: function(e) {
+            window.swellnetElapsedTime += this.getPosition();
+        }
+    }
+};
 
 //-----------------------------------------------------------------
 // INIT
 //-----------------------------------------------------------------
 
-getDates();
+//getDates();
+playVideo('rtmp://streamer.swellnet.com.au/surfcams/knights.stream',
+          'https://streamer.swellnet.com.au/surfcams/knights.stream/playlist.m3u8',
+          'http://static.swellnet.com.au/images/surfcams/knights.jpg');
 
 //-----------------------------------------------------------------
 //
 //-----------------------------------------------------------------
 
-function getDates(){
-	// $dateSelector.load('http://ajax-endpoint', function(){
-		// populate selector
-	// });
-}
+// function getDates(){
+// 	// $dateSelector.load('http://ajax-endpoint', function(){
+// 		// populate selector
+// 	// });
+// }
 
 //-----------------------------------------------------------------
 // SELECT DATE
 //-----------------------------------------------------------------
 
-function selectDate(){
-	// $dateSelector.on('change', function(){
-		// load latest 100 from that date
-		// find closest instance of thumbnails
-		// render them in
-	// });
-}
+// function selectDate(){
+// 	// $dateSelector.on('change', function(){
+// 		// load latest 100 from that date
+// 		// find closest instance of thumbnails
+// 		// render them in
+// 	// });
+// }
 
 //-----------------------------------------------------------------
 // LOAD REPLAY THUMBS
 //-----------------------------------------------------------------
 
-function loadReplayThumbs(){
+// function loadReplayThumbs(){
+// 	// if replay thumbs is 'data-replay-mode' => find accompanying video player and load it into JPlayer
+// 	// else populate the thumbs with links instead
+// }
 
+//-----------------------------------------------------------------
+// PLAY VIDEO
+//-----------------------------------------------------------------
+
+function playVideo(stream, playlist, image) {
+	var newStream = {
+	    sources: [
+	        {file: stream   },   // 'rtmp://streamer.swellnet.com.au/surfcams/knights.stream'
+	        {file: playlist }, // https://streamer.swellnet.com.au/surfcams/knights.stream/playlist.m3u8
+	    ],
+	    image: image,         // 'http://static.swellnet.com.au/images/surfcams/knights.jpg'
+	};
+	jQuery.extend(player_conf, newStream);
+	jwplayer('video').setup(player_conf);
 }
 
-//-----------------------------------------------------------------
-//
-//-----------------------------------------------------------------
 //-----------------------------------------------------------------
 //
 //-----------------------------------------------------------------
